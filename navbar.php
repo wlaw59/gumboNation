cc0af1881a79390594908a3cfb4a317226afcdc8
wlaw59 <52328188+wlaw59@users.noreply.github.com>
2021-04-26 17:30:00 -0600

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,43 +1,35 @@
-
-<html>
 <head>
-
-<link rel="icon" type="image/jpg" href="logo4.jpg"/>
-
-<link rel="stylesheet" type="text/css" href="style.css">
 <title>Gumbo Nation!</title>
-<!sign up, login page 59 ---59>
+<link rel="icon" type="image/jpg" href="logo4.jpg">
+
+<link rel="stylesheet" href="style.css">
 
 </head>
-<!--end of the signup/login-->
-<body>
+
 <div class="signup">
-	<ul>
-		<li>
 		<a href="login.php"> Login </a>
-		</li>
-		<li>
 		<a href="create_account.php"> Sign-Up </a>
-		</li>
-	</ul>
 </div>
-<!--logo goes to the home page  did it work59-->
+
+<div class="logout">
+		<a href="logout.php"> Logout </a>		
+</div>
+
+<div class="carousel">
+		<a href="carousel.php"> carousel </a>		
+</div>
+
 <div class ="banner">
 	<a id="logo" href="index.php"><img src="logo4.jpg" alt="Gumbo Nation"></a>
 </div>
 
 <div class="navbar">
 	<ul>
-		<li class="nav buttons">
+		<li class="navbuttons">
 			<a href="consultation.php"> Consultation </a>
 		</li>
-		<li class="nav buttons" >
+		<li class="navbuttons" >
 			<a href ="artinovation.php"> Art/Inovation </a>
 		</li>		
 	</ul>
 </div>
-
-
-
-
-
